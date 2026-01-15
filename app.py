b8492f09b36d5f50cf422fd2ca64b156f0926395
yashhh-dev <indoyash27@gmail.com>
2026-01-15 09:07:32 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ async def EncRypTMajoRLoGin(open_id, access_token):
     major_login.event_time = str(datetime.now())[:-7]
     major_login.game_name = "free fire"
     major_login.platform_id = 1
-    major_login.client_version = "1.118.1"
+    major_login.client_version = "1.120.2"
     major_login.system_software = "Android OS 9 / API-28 (PQ3B.190801.10101846/G9650ZHU2ARC6)"
     major_login.system_hardware = "Handheld"
     major_login.telecom_operator = "Verizon"
